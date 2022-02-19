5b837e7d85ae50329758cbe7aa0c515b83046b25
aa0s0mface <72880941+TheSman122333@users.noreply.github.com>
2022-02-19 10:18:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let counter = document.querySelector('h1');
+let count = 1;
+setInterval(()=>{
+    //counter.innerText = count;
+    count++
+    
+    if(count > 2) location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    
+},1000)
