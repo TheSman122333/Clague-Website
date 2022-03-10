b683d903a73a85608134ce7f5f11afacd277e366
aa0s0mface <72880941+TheSman122333@users.noreply.github.com>
2022-03-09 19:54:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ setInterval(()=>{
     //counter.innerText = count;
     count++
     
-    /*if(count > 2) location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')*/
+    if(count > 2) location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     
 },1000)
